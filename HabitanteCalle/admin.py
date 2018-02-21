8a9ee9e541265567c0829a0749926c610f478e8b
jfsoto1999@outlook.com <you@example.com>
2018-02-21 15:17:21 -0500

    segundo commit jhon soto

--- HabitanteCalle/admin.py
+++ HabitanteCalle/admin.py
@@ -6,18 +6,56 @@ class UsuarioAdmin(admin.ModelAdmin):
                    'segundo_apellido','numero_documento','telefono_movil',
                    'pais_procedencia','departamento_procedencia','municipio_procedencia','ocupacion',
                    )
+    filter_horizontal = ('educacion_desarrollo_humano',)
+    search_fields = ('numero_documento','primer_nombre','segundo_nombre','primer_apellido','segundo_apellido')
+    fieldsets = (
+        ('INFORMACION BASICA',{
+            'fields':('primer_nombre','segundo_nombre','primer_apellido',
+                   'segundo_apellido','tipo_documento_identidad','numero_documento','sexo','genero','fecha_nacimiento',)
+        }),
+        ('INFORMACION DE UBICACION Y CONTACTO', {
+            'fields': ('lugar_nacimiento', 'telefono_fijo', 'telefono_movil',
+                       'correo_electronico', 'pais_procedencia', 'departamento_procedencia', 'municipio_procedencia', 'zona_ubicacion',
+                       'barrio','direccion','estrato','comuna','vereda','corregimiento',)
+        }),
+        ('INFORMACION DE NUCLEO FAMILIAR', {
+            'fields': ('estado_civil', 'numero_hijos', 'cabeza_jefe_familia',)
+        }),
+        ('INFORMACION DE EDUCACION Y OCUPACION', {
+            'fields': ('ocupacion', 'nivel_escolaridad', 'estudios_completos','educacion_desarrollo_humano',)
+        }),
+        ('INFORMACION COMUNITARIA', {
+            'fields': ('etnia', 'comunidad_indigena', 'grupo', 'grupo_poblacional',)
+        }),
+        ('DISCAPACIDADES', {
+            'fields': ('posee_discapacidad', 'cual_discapasidad',)
+        }),
+    )
 
 
 class PaisAdmin(admin.ModelAdmin):
     list_display = ('pais',)
+    ordering = ('pais',)
+    search_fields = ('pais',)
 
 class DepartamentoAdmin(admin.ModelAdmin):
     list_display = ('departamento',)
+    ordering = ('departamento',)
+    search_fields = ('departamento',)
 
 class MunicipioAdmin(admin.ModelAdmin):
     list_display = ('municipio',)
+    ordering = ('municipio',)
+    search_fields = ('municipio',)
+
+class EstudiosAdmin(admin.ModelAdmin):
+    list_display = ('estudio',)
+    ordering = ('estudio',)
+    search_fields = ('estudio',)
+
 
 admin.site.register(models.Usuario,UsuarioAdmin)
 admin.site.register(models.Pais,PaisAdmin)
 admin.site.register(models.Departamento,DepartamentoAdmin)
 admin.site.register(models.Municipio,MunicipioAdmin)
+admin.site.register(models.Estudios,EstudiosAdmin)
--- HabitanteCalle/models.py
+++ HabitanteCalle/models.py
@@ -2,8 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 TIPO_DOCUMENTO_CHOICES=(
-    ('Cédula de Ciudadanía','Cédula de Ciudadanía'),
-    ('Cédula de Extranjería', 'Cédula de Extranjería'),
+    ('Cedula de Ciudadania','Cedula de Ciudadania'),
+    ('Cedula de Extranjeria', 'Cedula de Extranjeria'),
     ('Tarjeta de Identidad','Tarjeta de Identidad'),
 )
 SEXO=(
@@ -42,14 +42,11 @@ NIVEL_ESCOLARIDAD=(
     ('Secundaria','Secundaria'),
     ('Universitaria','Universitaria'),
 )
-ESTUDIOS_DESARROLLO_HUMANO=(
-    ('Educaccion para el Trabajo','Educaccion para el Trabajo'),
-    ('Desarrollo Humano','Desarrollo Humano'),
-)
+
 ETNIA=(
     ('Afro','Afro'),
     ('Mestizo','Mestizo'),
-    ('Indígena','Indígena'),
+    ('Indigena','Indigena'),
 )
 PERTENECE_GRUPO=(
     ('Poblacionales','Poblacionales'),
@@ -60,60 +57,68 @@ DISCAPACIDAD=(
     ('Auditiva','Auditiva'),
     ('Visual','Visual'),
     ('Cognitiva','Cognitiva'),
-    ('Física','Física')
+    ('Fisica','Fisica')
 )
 
 class Pais(models.Model):
     pais=models.CharField(max_length=100,blank=True,null=True)
-    def __str__(self):
+    def __unicode__(self):
         return self.pais
 
 class Departamento(models.Model):
     departamento=models.CharField(max_length=100,blank=True,null=True)
-    def __str__(self):
+    def __unicode__(self):
         return self.departamento
 
 class Municipio(models.Model):
     municipio=models.CharField(max_length=100,blank=True,null=True)
-    def __str__(self):
+    def __unicode__(self):
         return self.municipio
 
+class Estudios(models.Model):
+    estudio=models.CharField(max_length=100,blank=True,null=True,)
+    def __unicode__(self):
+        return self.estudio
+
 class Usuario(models.Model):
-    primer_nombre=models.CharField(max_length=60)
-    segundo_nombre = models.CharField(max_length=60,blank=True,null=True)
-    primer_apellido = models.CharField(max_length=60)
-    segundo_apellido = models.CharField(max_length=60,blank=True,null=True)
-    tipo_documento_identidad=models.CharField(choices=TIPO_DOCUMENTO_CHOICES,max_length=100)
-    numero_documento=models.CharField(max_length=20)
-    sexo=models.CharField(choices=SEXO,max_length=100)
-    genero=models.CharField(choices=GENERO,blank=True,null=True,max_length=100)
-    fecha_nacimiento=models.DateField(blank=True,null=True)
-    lugar_nacimiento=models.CharField(max_length=60,blank=True,null=True)
-    telefono_fijo=models.CharField(max_length=20, blank=True,null=True)
-    telefono_movil = models.CharField(max_length=20, blank=True,null=True)
-    correo_electronico=models.EmailField(max_length=60,blank=True,null=True)
-    pais_procedencia=models.ForeignKey(Pais,on_delete=models.CASCADE,null=True,blank=True)
-    departamento_procedencia=models.ForeignKey(Departamento,on_delete=models.CASCADE,null=True,blank=True)
-    municipio_procedencia=models.ForeignKey(Municipio,on_delete=models.CASCADE,null=True,blank=True)
-    zona_ubicacion=models.CharField(choices=UBICACION,blank=True,null=True,max_length=100)
+    primer_nombre=models.CharField(verbose_name='Pimer Nombre',max_length=60)
+    segundo_nombre = models.CharField(verbose_name='Segundo Nombre',max_length=60,blank=True,null=True)
+    primer_apellido = models.CharField(verbose_name='Pimer Apellido',max_length=60)
+    segundo_apellido = models.CharField(verbose_name='Segundo Apellido',max_length=60,blank=True,null=True)
+    tipo_documento_identidad=models.CharField(verbose_name='Tipo de Documento de Identidad',choices=TIPO_DOCUMENTO_CHOICES,max_length=100)
+    numero_documento=models.CharField(verbose_name='Numero de Documento de Identidad',max_length=20)
+    sexo=models.CharField(verbose_name='Sexo',choices=SEXO,max_length=100)
+    genero=models.CharField(verbose_name='Genero',choices=GENERO,blank=True,null=True,max_length=100)
+    fecha_nacimiento=models.DateField(verbose_name='Fecha de Nacimiento',blank=True,null=True)
+    lugar_nacimiento=models.CharField(verbose_name='Lugar de Nacimiento',max_length=60,blank=True,null=True)
+    telefono_fijo=models.CharField(verbose_name='Telefono Fijo',max_length=20, blank=True,null=True)
+    telefono_movil = models.CharField(verbose_name='Telefono Movil',max_length=20, blank=True,null=True)
+    correo_electronico=models.EmailField(verbose_name='Correo Electronico',max_length=60,blank=True,null=True)
+    pais_procedencia=models.ForeignKey(Pais,verbose_name='Pais de Prosedencia',on_delete=models.CASCADE,null=True,blank=True)
+    departamento_procedencia=models.ForeignKey(Departamento,verbose_name='Departamento de Prosedencia',on_delete=models.CASCADE,null=True,blank=True)
+    municipio_procedencia=models.ForeignKey(Municipio,verbose_name='Municipio de Prosecendencia',on_delete=models.CASCADE,null=True,blank=True)
+    zona_ubicacion=models.CharField(verbose_name='Zona de Ubicacion',choices=UBICACION,blank=True,null=True,max_length=100)
     barrio=models.CharField(max_length=60,blank=True)
     direccion=models.CharField(max_length=60,blank=True)
     estrato=models.CharField(max_length=20,blank=True)
     comuna =models.CharField(max_length=60,blank=True)
     vereda=models.CharField(max_length=60,blank=True)
     corregimiento=models.CharField(max_length=60,blank=True)
-    estado_civil=models.CharField(choices=ESTADO_CIVIL,blank=True,null=True,max_length=100)
-    numero_hijos=models.CharField(max_length=20,blank=True)
+    estado_civil=models.CharField(verbose_name='Estado Civil',choices=ESTADO_CIVIL,blank=True,null=True,max_length=100)
+    numero_hijos=models.CharField(verbose_name='Numero de Hijos',max_length=20,blank=True)
     cabeza_jefe_familia=models.CharField(choices=CABEZA_JEFE_DE_FAMILIA,blank=True,null=True,max_length=100,verbose_name='Es cabeza de familia o jefe(a) de hogar')
     ocupacion=models.CharField(choices=OCUPACION,blank=True,null=True,max_length=100)
-    nivel_escolaridad=models.CharField(choices=NIVEL_ESCOLARIDAD,blank=True,null=True,max_length=100)
-    estudios_completos=models.BooleanField(blank=True,verbose_name='Realizó sus estudios de manera completa')
-    educacion_desarrollo_humano=models.CharField(choices=ESTUDIOS_DESARROLLO_HUMANO,blank=True,null=True,max_length=100,verbose_name='Ha llevado a cabo alguno de estos estudios de Educación para el Trabajo y el Desarrollo Humano')
-    etnia=models.CharField(choices=ETNIA,null=True,blank=True,max_length=100,verbose_name='Autoreconocimiento étnico')
-    comunidad_indigena=models.CharField(max_length=60,blank=True,null=True,verbose_name='Si su respuesta es indígena indique a qué comunidad pertenece')
+    nivel_escolaridad=models.CharField(verbose_name='Nivel de Escolaridad',choices=NIVEL_ESCOLARIDAD,blank=True,null=True,max_length=100)
+    estudios_completos=models.BooleanField(blank=True,verbose_name='Realizo sus estudios de manera completa')
+    educacion_desarrollo_humano=models.ManyToManyField(Estudios,blank=True,max_length=100,verbose_name='Ha llevado a cabo alguno de estos estudios de Educacion para el Trabajo y el Desarrollo Humano')
+    etnia=models.CharField(choices=ETNIA,null=True,blank=True,max_length=100,verbose_name='Autoreconocimiento etnico')
+    comunidad_indigena=models.CharField(max_length=60,blank=True,null=True,verbose_name='Si su respuesta es indigena indique a quee comunidad pertenece')
     grupo=models.CharField(choices=PERTENECE_GRUPO,blank=True,null=True,max_length=100,verbose_name='Pertenece actualmente a alguno de estos grupos')
-    grupo_poblacional=models.CharField(max_length=100,blank=True,null=True,verbose_name='cuál?')
+    grupo_poblacional=models.CharField(max_length=100,blank=True,null=True,verbose_name='cual?')
     posee_discapacidad=models.BooleanField(blank=True,verbose_name='Posee alguna discapacidad')
-    cual_discapasidad=models.CharField(choices=DISCAPACIDAD,blank=True,null=True,max_length=100,verbose_name='Cuál?')
+    cual_discapasidad=models.CharField(choices=DISCAPACIDAD,blank=True,null=True,max_length=100,verbose_name='Cual?')
+
+    def __unicode__(self):
+        return self.numero_documento
 
 
